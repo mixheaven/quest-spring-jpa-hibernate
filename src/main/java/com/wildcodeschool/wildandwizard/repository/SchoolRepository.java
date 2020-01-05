3c74413b67ee07507382a068b4d022a274aa328c
mixheaven <mixheaven@hotmail.com>
2020-01-05 15:43:17 +0100

    final shoot

--- src/main/java/com/wildcodeschool/wildandwizard/repository/SchoolRepository.java
+++ src/main/java/com/wildcodeschool/wildandwizard/repository/SchoolRepository.java
@@ -1,6 +1,11 @@
 package com.wildcodeschool.wildandwizard.repository;
 
+import com.wildcodeschool.wildandwizard.entity.School;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
 // TODO : update this interface
 
-public interface SchoolRepository {
+public interface SchoolRepository extends JpaRepository<School, Long> {
+
 }
